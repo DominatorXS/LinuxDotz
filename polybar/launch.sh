387ff170bd6847f8bdb4bb4713486e16372f45e2
DominatorXS <114426813+DominatorXS@users.noreply.github.com>
2023-01-01 20:28:22 +0530

    Delete polybar directory

--- polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-killall -q polybar
-
-echo "---" | tee -a /tmp/polybar.log
-polybar main 2>&1 | tee -a /tmp/polybar.log & disown
-polybar main2 2>&1 | tee -a /tmp/polybar.log & disown
-polybar main3 2>&1 | tee -a /tmp/polybar.log & disown
-echo "Bars launched..."
